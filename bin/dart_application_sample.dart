257c159993f2af8ebd6f886aeb43d78af3a138be
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-10-02 14:54:21 +0700

    Initial Project

---
+++ bin/dart_application_sample.dart
@@ -0,0 +1,6 @@
+import 'package:dart_application_sample/dart_application_sample.dart'
+    as dart_application_sample;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_sample.calculate()}!');
+}
---
+++ test/dart_application_sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_sample/dart_application_sample.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
