2cebb837d946a19c67f6cad11d33ec1154bd4c2c
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-10-02 14:59:14 +0700

    Change to hello everyone

--- bin/dart_application_sample.dart
+++ bin/dart_application_sample.dart
@@ -2,5 +2,5 @@ import 'package:dart_application_sample/dart_application_sample.dart'
     as dart_application_sample;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_sample.calculate()}!');
+  print('Hello everyone');
 }
